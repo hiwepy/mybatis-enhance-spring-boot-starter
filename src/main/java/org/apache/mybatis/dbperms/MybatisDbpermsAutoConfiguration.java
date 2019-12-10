b271b510a7f0921a139bb9a4a7ecbf22b2bf0e00
wandl <hnxyhcwdl1003@163.com>
2019-12-10 18:45:52 +0800

    xg

--- src/main/java/org/apache/mybatis/dbperms/MybatisDbpermsAutoConfiguration.java
+++ src/main/java/org/apache/mybatis/dbperms/MybatisDbpermsAutoConfiguration.java
@@ -8,37 +8,56 @@ import org.apache.mybatis.dbperms.parser.ITablePermissionAnnotationHandler;
 import org.apache.mybatis.dbperms.parser.ITablePermissionAutowireHandler;
 import org.apache.mybatis.dbperms.parser.TablePermissionAnnotationParser;
 import org.apache.mybatis.dbperms.parser.TablePermissionAutowireParser;
+import org.springframework.beans.BeansException;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConditionalOnClass({ Plugin.class, DataPermissionStatementInterceptor.class })
 @EnableConfigurationProperties(MybatisDbpermsProperties.class)
-public class MybatisDbpermsAutoConfiguration {
+public class MybatisDbpermsAutoConfiguration implements ApplicationContextAware {
+	
+	private ApplicationContext applicationContext;
 	
     @Bean
     @ConditionalOnMissingBean
-    public ITablePermissionAutowireHandler tablePermissionAutowireHandler() {
-    	return new DefaultTablePermissionAutowireHandler((metaHandler, tableName) -> {
-    		return null;
-    	});
+    public TablePermissionAutowireParser autowirePermissionParser() {
+    	
+    	TablePermissionAutowireParser autowirePermissionParser = new TablePermissionAutowireParser() {
+    		
+    		@Override
+    		protected void internalInit() {
+    			
+    			super.internalInit();
+    			
+    			ITablePermissionAutowireHandler tablePermissionHandler = super.getTablePermissionHandler();
+    			
+    			if(null == tablePermissionHandler) {
+    				tablePermissionHandler = getApplicationContext().getBean(ITablePermissionAutowireHandler.class);
+    			}
+    			if(null != tablePermissionHandler) {
+    				tablePermissionHandler = new DefaultTablePermissionAutowireHandler((metaHandler, tableName) -> {
+	            		return null;
+	            	});
+				}
+    			
+    			this.setTablePermissionHandler(tablePermissionHandler);
+    		}
+    		
+    	};
+    	 
+    	return autowirePermissionParser;
     }
     
     @Bean
     @ConditionalOnMissingBean
     public ITablePermissionAnnotationHandler tablePermissionAnnotationHandler() {
     	return new DefaultTablePermissionAnnotationHandler();
-    }
-    
-    @Bean
-    @ConditionalOnMissingBean
-    public TablePermissionAutowireParser autowirePermissionParser(ITablePermissionAutowireHandler tablePermissionHandler) {
-    	TablePermissionAutowireParser autowirePermissionParser = new TablePermissionAutowireParser();
-    	autowirePermissionParser.setTablePermissionHandler(tablePermissionHandler);
-    	return autowirePermissionParser;
     }
     
     @Bean
@@ -55,5 +74,15 @@ public class MybatisDbpermsAutoConfiguration {
     		TablePermissionAnnotationParser annotationPermissionParser) {
         return new DataPermissionStatementInterceptor(autowirePermissionParser, annotationPermissionParser);
     }
+
+	
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+	
+	public ApplicationContext getApplicationContext() {
+		return applicationContext;
+	}
 	
 }
